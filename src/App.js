3377652453a5d6e8bc2f0dcecfbf953c68eca9c2
Christian Ani <iamchristianani@gmail.com>
2023-01-10 18:08:11 +0100

    Update App.js from function to class component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
+import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
 
 export default App;
