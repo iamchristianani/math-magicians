7cffe538714baa86bf6f80ae8cb096dcdd512df2
Christian Ani <iamchristianani@gmail.com>
2023-01-18 10:21:31 +0100

    Create a function based navbar component

---
+++ src/components/Navbar.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Nav = () => (
+  <header className="nav-bar">
+    <h1 className="logo"> Math Magicians </h1>
+    <ul className="nav-links">
+      <li>
+        <Link to="/">Home</Link>
+      </li>
+      <li>
+        <Link to="/calculator">Calculator</Link>
+      </li>
+      <li>
+        <Link to="/quote">Quote</Link>
+      </li>
+    </ul>
+  </header>
+);
+
+export default Nav;
