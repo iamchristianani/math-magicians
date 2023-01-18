a459b721217f2d9b1ebcedd74812038552729299
Christian Ani <iamchristianani@gmail.com>
2023-01-18 10:21:06 +0100

    Create a function based home component

---
+++ src/components/Home.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+export default function Home() {
+  return (
+    <div className="home">
+      <h2> Welcome to our page! </h2>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
+        vestibulum erat. Nulla sagittis massa at egestas facilisis. Quisque
+        tincidunt interdum vulputate. Sed orci orci, viverra vel tincidunt vel,
+        consectetur eget tortor. Integer pellentesque eros vel ex maximus, quis
+        aliquet nisl tristique. Nulla lobortis sem a placerat maximus. Duis
+        suscipit purus at nunc bibendum, et finibus quam tempor. Donec sed magna
+        suscipit, bibendum nisi faucibus, dictum erat. Aenean varius eros vitae
+        arcu tempus lobortis. Ut et nisl eget purus ornare tincidunt. Aliquam erat
+        volutpat. Suspendisse vitae finibus odio. Suspendisse aliquam risus massa,
+        vel vulputate mauris tempus a.
+
+      </p>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam sed
+        vestibulum erat. Nulla sagittis massa at egestas facilisis. Quisque
+        tincidunt interdum vulputate. Sed orci orci, viverra vel tincidunt vel,
+        consectetur eget tortor. Integer pellentesque eros vel ex maximus, quis
+        aliquet nisl tristique. Nulla lobortis sem a placerat maximus. Duis
+        suscipit purus at nunc bibendum, et finibus quam tempor. Donec sed magna
+        suscipit, bibendum nisi faucibus, dictum erat. Aenean varius eros vitae
+        arcu tempus lobortis. Ut et nisl eget purus ornare tincidunt. Aliquam erat
+        volutpat. Suspendisse vitae finibus odio. Suspendisse aliquam risus massa,
+        vel vulputate mauris tempus a.
+
+      </p>
+    </div>
+  );
+}
